ca509a78bba3cb08393884303585b561fb235850
renu28choudhary <renu28choudhary@gmail.com>
2023-04-11 17:23:28 -0400

    Little lemon functionality

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,39 @@
-import './App.css';
-import Header from './Header'
-import Nav from './Nav'
-import Footer from './Footer'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Booking from "./components/Booking";
+import BookingConfirmation from "./components/BookingConfirmation";
+import { useFormContext } from "./store/FormContext";
+import "./App.css";
 
 function App() {
-  return (
-    //< className="App">
-    <>
-      <Header/>
-      <Nav/>
-      <Footer/>
+  const { form } = useFormContext();
 
-    </>
-    
-    
+  const formProps = {
+    name: form.name,
+    date: form.date,
+    time: form.time,
+    guests: form.numberOfGuests,
+    occasion: form.occasion,
+    table: form.tablePreference,
+    request: form.message,
+  };
+
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route
+          path="/booking-confirmation"
+          element={<BookingConfirmation {...formProps} />}
+        />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
